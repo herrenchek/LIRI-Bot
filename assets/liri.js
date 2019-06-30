b3d6ccb4e8ca88c5de898a20a6d7fd5c2309f500
herrenchek <giselle.herr@icloud.com>
2019-06-30 17:21:24 -0400

    Set up switch statements

--- assets/liri.js
+++ assets/liri.js
@@ -11,3 +11,21 @@ console.log(keys);
 
 //node liri.js concert-this <artist/band name here>
 
+var command = process.argv[2];
+
+switch (command) {
+    case 'concert-this':
+        console.log('Oranges are $0.59 a pound.');
+        break;
+    case 'spotify-this-song':
+        console.log('Oranges are $0.59 a pound.');
+        break;
+    case 'movie-this':
+        console.log('Oranges are $0.59 a pound.');
+        break;
+    case 'do-what-it-says':
+        console.log('Oranges are $0.59 a pound.');
+        break;
+    default:
+        console.log('Sorry, I don\'t recognize that command.');
+}
