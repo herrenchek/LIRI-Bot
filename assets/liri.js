0231801b03b99851895f3792749cc0e44a9a430d
herrenchek <giselle.herr@icloud.com>
2019-06-30 17:02:03 -0400

    Starting concert-this

--- assets/liri.js
+++ assets/liri.js
@@ -1,5 +1,13 @@
+//LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies
+
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
 
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+console.log(keys);
+
+//node liri.js concert-this <artist/band name here>
+
